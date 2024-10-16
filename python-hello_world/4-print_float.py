35b15a65e582353f430d2c76392206026ad07612
isamuella <s.ineza@alustudent.com>
2024-10-16 06:55:56 -0700

    float

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -3,6 +3,6 @@ number = 3.14159
 
 try:
     number+1
-print(f"Float: {number:.2f}")
+    print(f"Float: {number:.2f}")
 except:
     print("ValueError: Unknown format code 'f' for object of type 'str'")
