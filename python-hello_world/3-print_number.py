df469340cfef09554fc44bd144842b3d80d56bcd
isamuella <s.ineza@alustudent.com>
2024-10-16 05:43:41 -0700

    printnum

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -4,4 +4,4 @@ try:
     number+1
     print(f"{number} Battery street")
 except:
-    print("ValueError: unknown format code 'd' for object of type 'str')
+    print("ValueError: unknown format code 'd' for object of type 'str'")
