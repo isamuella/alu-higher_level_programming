462588c698296726821a113ab8c82fac604dad07
isamuella <s.ineza@alustudent.com>
2024-10-17 05:53:39 -0700

    function for last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -6,5 +6,5 @@ def print_last_digit(number):
         last_digit = abs(int(number)) % 10
         print(last_digit, end="")
         return last_digit
-    except ValueError: 
+    except ValueError:
         return "Traceback (most recent call last):"
