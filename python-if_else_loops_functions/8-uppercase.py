fef790ca4ddf0c072419c36d35ec28229dd89b47
isamuella <s.ineza@alustudent.com>
2024-10-17 04:59:48 -0700

    uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    a = ord('a')
+    b = ord('z')
+    for char in str:
+        if a <= ord(char) <= b:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print()
