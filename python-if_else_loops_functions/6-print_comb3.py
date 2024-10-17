60e9fda04d4571836d3e8fd9c909cf0daa3c0a3b
isamuella <s.ineza@alustudent.com>
2024-10-17 04:41:12 -0700

    different combinations

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for num1 in range(10):
     for num2 in range(num1 + 1, 10):
-        if num1 == 3 and num2 == 4:
+        if num1 == 8 and num2 == 9:
             print("{:d}{:d}".format(num1, num2))
         else:
-            print("TextError:wrong numbers", end="")
+            print("{:d}{:d}, ".format(num1, num2), end="")
