3c856184799c6c80b0a026c65660603003283d3a
isamuella <s.ineza@alustudent.com>
2024-10-17 05:34:51 -0700

    function for last digit

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    try:
+        number = int(number)*1
+        last_digit = str(number)[-1]
+        print("{}".format(last_digit), end="")
+        return last_digit
+    except:
+        return "Traceback:"
