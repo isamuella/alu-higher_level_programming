301dbf8fa32610ddca4a982702883f4f2f010417
isamuella <s.ineza@alustudent.com>
2024-10-22 04:15:22 -0700

    hidden_4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
 import hidden_4
 
- i = dir(hidden_4)
- new_i = [for name in i if not name.starting('__')]
+if __name__ == "__main__":
+    i = dir(hidden_4)
+    new_i = [for name in i if not name.starting('__')]
 
- for name in new_i:
-     print(name)
+    for name in new_i:
+        print(name)
