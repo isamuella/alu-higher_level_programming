71e22718fd3664cf7e8005a9b9ae03fdfb03210a
isamuella <s.ineza@alustudent.com>
2024-10-22 02:53:36 -0700

    toolbox

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,14 +2,14 @@
 
 from calculator_1 import add,sub,mul,div
 
-if __name__=="__main__":
+if __name__ == "__main__":
     a = 10
     b = 5
     add = sum(a + b)
     subtract = sub(a - b)
     multiply = mul(a * b)
     divide = div(a / b)
-    print("{} + {}".format(a, b, add))
-    print("{} - {}".format(a, b, subtract))
-    print("{} * {}".format(a, b, multiply))
-    print("{} / {}".format(a, b, divide))
+    print("{} + {} = {}".format(a, b, add))
+    print("{} - {} = {}".format(a, b, subtract))
+    print("{} * {} = {}".format(a, b, multiply))
+    print("{} / {} = {}".format(a, b, divide))
