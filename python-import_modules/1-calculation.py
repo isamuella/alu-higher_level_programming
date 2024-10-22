330ef21cba910afe30db6a224d279654f780f296
isamuella <s.ineza@alustudent.com>
2024-10-22 02:56:41 -0700

    toolbox

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,10 +5,10 @@ from calculator_1 import add,sub,mul,div
 if __name__ == "__main__":
     a = 10
     b = 5
-    add = sum(a + b)
-    subtract = sub(a - b)
-    multiply = mul(a * b)
-    divide = div(a / b)
+    add = sum(a, b)
+    subtract = sub(a, b)
+    multiply = mul(a, b)
+    divide = div(a, b)
     print("{} + {} = {}".format(a, b, add))
     print("{} - {} = {}".format(a, b, subtract))
     print("{} * {} = {}".format(a, b, multiply))
