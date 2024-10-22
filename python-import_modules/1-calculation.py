859e6abbd1e806f288ba3bda4ead5f9b82fa23c6
isamuella <s.ineza@alustudent.com>
2024-10-22 02:44:04 -0700

    toolbox

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     a = 10
     b = 5
     sum = a + b
-    print("{} + {}".format(a, b, sum))
     sub = a - b
-    print("{} - {}".format(a, b, sub))
     mul = a * b
-    print("{} * {}".format(a, b, mul))
     div = a / b
+    print("{} + {}".format(a, b, sum))
+    print("{} - {}".format(a, b, sub))
+    print("{} * {}".format(a, b, mul))
     print("{} / {}".format(a, b, div))
