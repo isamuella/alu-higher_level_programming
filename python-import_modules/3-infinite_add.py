ec32c8eec3635f57f6676880be6c1262c28db4eb
isamuella <s.ineza@alustudent.com>
2024-10-22 03:47:25 -0700

    add infinitely

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 from sys import argv
+
 if __name__ == "__main__":
-    sum_args = sum(int(args) for args in argv[1:])
-    print("{}".format(sum_args))
+    arguments = []
+    for arg in argv[1:]:
+        arguments.append(int(arg))
+    sum_arg = sum(arguments)
+    print("{}".format(sum_arg))
