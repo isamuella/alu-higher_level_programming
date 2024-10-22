eb6187ae881b073d81f41577eb72bab8912107dd
isamuella <s.ineza@alustudent.com>
2024-10-22 03:21:23 -0700

    arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+arg_num = len(argv)-1
+if arg_num == 0:
+    print("{} arguments.".format(arg_num))
+    elif arg_num == 1:
+        print("{} arguments:.format"(arg_num))
+    else:
+        print("{} argumrnts:".format(arg_num))
+        
+        for num in range(1, len(argv)):
+            print("{}: {}".format(i, argv[num]))
+        
