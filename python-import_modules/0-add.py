d02f7ba2750e12fe47692d9bbb0213da3d0fbf84
isamuella <s.ineza@alustudent.com>
2024-10-22 02:20:05 -0700

    import a function from a file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 
 from add_0 import add
 if __name__ == "__main__":
-a = 1
-b = 2
-sum = add(a, b)
-print("{} + {} = {}".format(a, b, sum))
+    a = 1
+    b = 2
+    sum = add(a, b)
+    print("{} + {} = {}".format(a, b, sum))
