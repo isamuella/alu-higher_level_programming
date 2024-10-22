68f14950ac32b54dac8dd2956dd001f380294732
isamuella <s.ineza@alustudent.com>
2024-10-22 02:32:33 -0700

    toolbox

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from calculator_1 import math
+
+if __name__ = "__main__":
+    a = 10
+    b = 5
+    if sum = a + b
+    print("{} + {}".format(a, b))
+    elif subtr = a - b
+    print("{} - {}".format(a, b))
+    elif mult = a * b
+    print("{} * {}".format(a, b))
+    else div = a / b
+    print("{} / {}".format(a, b))
+
