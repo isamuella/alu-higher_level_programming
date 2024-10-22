e6af473801181cf069b6cf5a08c3d21d755a62a9
isamuella <s.ineza@alustudent.com>
2024-10-22 03:24:59 -0700

    arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+from sys import argv
 if __name__ == "__main__":
 arg_num = len(argv)-1
-if arg_num == 0:
+    if arg_num == 0:
     print("{} arguments.".format(arg_num))
     elif arg_num == 1:
         print("{} arguments:.format"(arg_num))
