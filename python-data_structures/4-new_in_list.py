ddf2266ae9e26a1671763881a61524feada3059d
isamuella <s.ineza@alustudent.com>
2024-10-23 01:49:56 -0700

    no modification

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -5,6 +5,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        new = my_list.copy()
+        new = my_list[:]
         new[idx] = element
         return new
