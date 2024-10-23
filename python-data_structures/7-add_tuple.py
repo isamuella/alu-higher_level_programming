c2379dd22a8c87e0af0a13aad3956d20a82984b8
isamuella <s.ineza@alustudent.com>
2024-10-23 02:39:38 -0700

    2 tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
 
-#!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Extract elements from the tuples, using 0 as a default for missing values
-    a1, a2 = (tuple_a + (0, 0))[:2]  # Default to 0 if tuple_a has less than 2 elements
-    b1, b2 = (tuple_b + (0, 0))[:2]  # Default to 0 if tuple_b has less than 2 elements
+    a1, a2 = (tuple_a + (0, 0))[:2]
+    b1, b2 = (tuple_b + (0, 0))[:2]
 
-    # Return the sum of the corresponding elements as a new tuple
     return (a1 + b1, a2 + b2)
