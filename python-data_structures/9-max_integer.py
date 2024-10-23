4307e66ef94efe76352b9f2c5811dc140f95f4e3
isamuella <s.ineza@alustudent.com>
2024-10-23 03:04:22 -0700

    max value

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    max_int = my_list[0]
+    for value in my_list:
+        if value > max_int:
+            max_int = value
+        return max_int
