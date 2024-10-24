4a90dcf589d949dbe214b7afea1e30a30371859b
isamuella <s.ineza@alustudent.com>
2024-10-24 04:54:07 -0700

    ex12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,19 +3,22 @@
 
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str)
-    return 0
-
-roman_vals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-result = 0
-prev_val = 0
-
-for char in reversed(roman_string):
-    curr_val = roman_vals.get(char, 0)
-
-    if curr_val >= prev_val:
-        result += curr_val
+        return 0
+    elif not roman_string:
+        return 0
     else:
-        result -= curr_val
 
-    prev_val = curr_val
-    return result
+         roman_vals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+         result = 0
+         prev_val = 0
+
+         for char in reversed(roman_string):
+             curr_val = roman_vals[char]
+
+             if curr_val >= prev_val:
+                result += curr_val
+             else:
+                result -= curr_val
+
+             prev_val = curr_val
+         return result
