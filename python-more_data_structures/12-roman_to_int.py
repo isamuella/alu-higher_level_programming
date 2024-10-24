464c8add21170edcb752dac9ec7b706269742f0e
isamuella <s.ineza@alustudent.com>
2024-10-24 05:04:37 -0700

    ex12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -8,8 +8,15 @@ def roman_to_int(roman_string):
         return 0
     else:
 
-        roman_vals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                       'C': 100, 'D': 500, 'M': 1000}
+        roman_vals = {
+                'I': 1,
+                'V': 5,
+                'X': 10,
+                'L': 50,
+                'C': 100,
+                'D': 500,
+                'M': 1000
+        }
         result = 0
         prev_val = 0
 
