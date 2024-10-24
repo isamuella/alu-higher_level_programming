a2951043bfbd4f522e19898e52f20350fc60ffd3
isamuella <s.ineza@alustudent.com>
2024-10-24 04:47:03 -0700

    ex12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str)
+    return 0
+
+roman_vals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+result = 0
+prev_val = 0
+
+for char in reversed(roman_string):
+    curr_val = roman_vals.get(char, 0)
+
+    if curr_val >= prev_val:
+        result += curr_val
+    else:
+        result -= curr_val
+
+    prev_val = curr_val
+    return result
