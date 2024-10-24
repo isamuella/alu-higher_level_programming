cedc64da1a3b4bc411449f7151ce5efa35343ebf
isamuella <s.ineza@alustudent.com>
2024-10-24 04:57:03 -0700

    ex12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,14 @@
 
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str)
+    if not isinstance(roman_string, str):
         return 0
     elif not roman_string:
         return 0
     else:
 
-         roman_vals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+         roman_vals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                       'C': 100, 'D': 500, 'M': 1000}
          result = 0
          prev_val = 0
 
