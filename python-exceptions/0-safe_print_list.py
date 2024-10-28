2bf3cdb5f85b3a4cbd36dff707470626de51ab31
isamuella <s.ineza@alustudent.com>
2024-10-28 07:09:48 -0700

    ex0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
@@ -9,5 +10,5 @@ def safe_print_list(my_list=[], x=0):
             print()
             return count
     except IndexError:
-        print("There's an Error")
+        print()
         return count
