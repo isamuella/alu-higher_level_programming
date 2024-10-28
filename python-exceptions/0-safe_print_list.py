4efd14d602eb59acf1ed0f149ff6f07402d0d505
isamuella <s.ineza@alustudent.com>
2024-10-28 07:30:48 -0700

    ex0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,8 +7,10 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             count += 1
+        print("")
+        return count
     except IndexError:
         pass    
     finally:
-        print()
+        print("")
      return count
