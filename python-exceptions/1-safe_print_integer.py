5664c7be15d8c08667d6f1fba2a262fa15cd4614
isamuella <s.ineza@alustudent.com>
2024-10-29 00:57:29 -0700

    ex1

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    count = 0
+    try:
+        for val in value:
+            print("Value: {:d},.format(int(value))")
+    except
+    return
