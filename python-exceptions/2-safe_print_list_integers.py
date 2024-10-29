78af4c7457c605c895e79c4e37cfe7d714fd55d0
isamuella <s.ineza@alustudent.com>
2024-10-29 01:20:21 -0700

    ex2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for item in my_list:
             if insistance(item, int)
-        print("{:d}".format (item))
+        print("{:d}".format(item))
         break
     except:
         return my_list
