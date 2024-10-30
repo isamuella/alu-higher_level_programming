5a76952bb6b5edbad02b77cda58db50b2ef7fa4c
isamuella <s.ineza@alustudent.com>
2024-10-30 05:24:59 -0700

    ex5

---
+++ python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+
+"""
+To create a class to define a square with a private attribute called size.
+"""
+
+
+class Square:
+    """
+    Class that defines a square.
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """creates a getter method for the __size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets a private attribute for the square size.
+
+        Raises:
+            TypeError: if value is not an integer.
+            ValueError: if value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
+    def my_print(self):
+        """prints the squre"""
+        if size = 0:
+            return end=""
