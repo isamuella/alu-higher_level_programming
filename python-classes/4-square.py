98f3cb21ab8ecf2acb537ac4fdd22d8a0cf3e0f8
isamuella <s.ineza@alustudent.com>
2024-10-30 04:46:44 -0700

    ex4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -12,13 +12,24 @@ class Square:
     """
 
     def __init__(self, size=0):
+        """
+        initializes the square's size through the setter method
+        """
         self.size = size
 
     @property
     def size(self):
+        """creates a getter method for the size attribute"""
         return self.__size
 
     @property.setter
+    """
+    Sets the square size.
+
+    raises:
+        ValueError: if value is less than 0.
+        TypeError: if value is not an integer.
+    """
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
