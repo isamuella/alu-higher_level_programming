2c09eeff930d443a53f2e4e30807998504f468c7
isamuella <s.ineza@alustudent.com>
2024-10-30 04:37:30 -0700

    ex4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -12,22 +12,14 @@ class Square:
     """
 
     def __init__(self, size=0):
-        self.size = size  # This will use the setter
+        self.size = size
 
     @property
     def size(self):
-        """Getter for the private attribute __size."""
         return self.__size
 
-    @size.setter
+    @property.setter
     def size(self, value):
-        """
-        Setter for the private attribute __size with validation.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -35,5 +27,5 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Calculate and return the area of the square."""
+        """Calculate the area of the square."""
         return self.__size ** 2
