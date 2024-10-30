01d74b3650a09ef35a9471e50b3c3b3d5154849f
isamuella <s.ineza@alustudent.com>
2024-10-30 02:59:37 -0700

    ex2

---
+++ python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""
+To create a class to define a square with a private attribute called size.
+"""
+
+
+class Square:
+    """
+    Class that defines a square.
+    """
+    def __init__(self, size=0):
+        try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            self.__size = size
+        except Exception as e:
+            raise e
+
