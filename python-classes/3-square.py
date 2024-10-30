bdfb8be547403eea58b1922ec917ac96ac525d70
isamuella <s.ineza@alustudent.com>
2024-10-30 04:00:54 -0700

    ex3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,7 +2,7 @@
 
 
 """
-To create a class to define a square with a private attribute called size and calculate the are of the square.
+To create a class to define a square with a private attribute called size.
 """
 
 
@@ -20,5 +20,5 @@ class Square:
         except Exception as e:
             raise e
 
-     def area(self):
-         return self.__size ** 2
+        def area(self):
+            return self.__size ** 2
