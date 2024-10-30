0c5a8be78875af86e651b8db7c847059701d0d56
isamuella <s.ineza@alustudent.com>
2024-10-30 02:14:49 -0700

    ex1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+"""This code creates a class Square and a private attribute size"""
+
 class Square:
+    """This code initializes the attribute size"""
     def __init__(self, size)
     self.size = __size
     pass
