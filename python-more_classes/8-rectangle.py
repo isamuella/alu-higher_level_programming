d66e0ae11afdcba6d87f5acda295bc6d6af8b653
isamuella <s.ineza@alustudent.com>
2024-11-01 03:58:01 -0700

    ex8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -79,7 +79,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
-    @static.method
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """
         Displays the biggest area.
