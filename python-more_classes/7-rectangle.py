21aed9254455f391247128f5b2188bf126372752
isamuella <s.ineza@alustudent.com>
2024-11-01 03:28:30 -0700

    ex7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -67,7 +67,8 @@ class Rectangle:
         """Print the rectangle with character #"""
         if self.__width == 0 or self.__height == 0:
             return ("")
-        return "\n".join(str(self.print_symbol) * self.__width for i in range(self.__height))
+        sign = str(self.print_symbol)
+        return "\n".join(sign * self.__width for i in range(self.__height))
 
     def __repr__(self):
         """A string representation"""
