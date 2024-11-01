0a9567cf5a7af7a853ded0adbac00ca38233798a
isamuella <s.ineza@alustudent.com>
2024-11-01 02:07:03 -0700

    ex1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
     @property
     def width(self):
         """created a getter method for the __width attribute"""
@@ -47,4 +48,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
