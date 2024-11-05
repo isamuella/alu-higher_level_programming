491c3b9910afbe4f8ff01c0b771875ccd3cf7908
isamuella <s.ineza@alustudent.com>
2024-11-05 02:36:10 -0800

    ex4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-""" """
+"""If is directly or indirectly an instance"""
 
 
 def inherits_from(obj, a_class):
-    """ """
+    """Return True if not an instance"""
     return isinstance(obj, a_class) and type(obj) is not a_class
