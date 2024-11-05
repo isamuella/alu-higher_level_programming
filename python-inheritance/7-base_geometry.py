c6b369724d793b9df85b046a5bcbd8d4a9b609d8
isamuella <s.ineza@alustudent.com>
2024-11-05 02:51:00 -0800

    ex7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""based on BaseGeometry"""
+
+
+class BaseGeometry:
+    """a class BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            return TypeError(f"<name> must be an integer")
+        if value <= 0:
+            return ValueError(f"<name> must be greater than 0")
