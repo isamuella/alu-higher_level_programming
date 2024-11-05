b96d35c4273aab4186fe6b9b4a2bd391f4dd3d53
isamuella <s.ineza@alustudent.com>
2024-11-05 03:28:08 -0800

    ex8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""based on BaseGeometry"""
+
+
+class BaseGeometry:
+    """a class BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+     
+class Rectangle(BaseGeometry):
+    """A child class"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
