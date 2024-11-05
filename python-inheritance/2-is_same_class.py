8ca406327f2fcc4ce1640413a40a7950135e1011
isamuella <s.ineza@alustudent.com>
2024-11-05 02:07:28 -0800

    ex2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Checking if object is exactly an instance of a specified class"""
+
+
+
+def is_same_class(obj, a_class):
+    """Returns true if object is exactly an instance of a specified class"""
+    print(isinsatnce(obj, a-class))
