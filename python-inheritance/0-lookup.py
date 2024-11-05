80c4be4865834e8b9a72d6303918b5900ab61dc7
isamuella <s.ineza@alustudent.com>
2024-11-05 00:46:29 -0800

    ex0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-"""Module for a lookup"""
+"""Module to lookup for a method."""
 
 def lookup(obj):
-"""Returns the list of available attributes and methods of an object"""
+    """Returns the list of available attributes and methods of an object."""
     return dir(obj)
