2db2e7cf70d6fbeedb710528bcce66942380bee2
isamuella <s.ineza@alustudent.com>
2024-11-05 02:44:23 -0800

    ex6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""based on BaseGeometry"""
+
+
+class BaseGeometry:
+    """a class BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
