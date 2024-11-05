878113fbda44517412758dae992d131bd4d88123
isamuella <s.ineza@alustudent.com>
2024-11-05 00:56:05 -0800

    ex1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A class MyList that inherits from list."""
+
+
+class MyList:
+    """The child class MyList"""
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
