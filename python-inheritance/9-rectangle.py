8e65ab36190f29725ee3ffbffe750e26e2c02944
isamuella <s.ineza@alustudent.com>
2024-11-06 07:35:56 -0800

    ex9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""based on BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A child class"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+     
+     def area(self):
+         """For finding area"""
+         return self.__width * self.__height
+
+     def __str__(self):
+         """for representing the string"""
+         return f"[Rectangle] {self.__width}/{self.__height}"
