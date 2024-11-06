2ab3a68bb9c6504a54f112814af10b434fa5cfc3
isamuella <s.ineza@alustudent.com>
2024-11-06 03:14:07 -0800

    ex1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,4 +7,4 @@ class MyList(list):
     """The child class MyList that inheerits from list"""
     def print_sorted(self):
         """prints the sorted list"""
-        print(sorted(self))
+        return sorted(self)
