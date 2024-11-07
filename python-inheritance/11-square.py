245f8216f678833809c802053094cb3fc1f363f8
isamuella <s.ineza@alustudent.com>
2024-11-07 01:43:17 -0800

    ex11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Based on Rectangle"""
+Rectangle = __import__(9-rectangle).Rectangle
+
+
+class Square(rectangle):
+    """A child class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """For representing the string"""
+        return f"[Square] {self.__width}/{self.__height}
