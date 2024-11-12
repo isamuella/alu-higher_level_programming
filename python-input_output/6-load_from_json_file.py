4242574b118a930aaa9b010f751c271326328750
isamuella <s.ineza@alustudent.com>
2024-11-12 05:36:50 -0800

    ex6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,4 +7,4 @@ import json
 def load_from_json_file(filename):
     """function to create an bject from json file"""
     with open(filename, mode="r", encoding="utf-8") as a_file:
-        json.load(a_file)
+        return json.load(a_file)
