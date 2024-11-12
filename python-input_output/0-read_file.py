c776af58292b1ddf72c0f50a8910ed4d9817a097
isamuella <s.ineza@alustudent.com>
2024-11-12 03:16:06 -0800

    ex0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Reading a text file"""
 
-
 def read_file(filename=""):
     """function to read the file filename"""
     with open(filename, mode="r", encoding="utf-8") as a_file:
