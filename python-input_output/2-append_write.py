1ef74d2698daa95b10fe08913d667fa6990f66df
isamuella <s.ineza@alustudent.com>
2024-11-12 03:36:19 -0800

    ex2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Append a string at the end of the text file"""
+
+
+def append_write(filename="", text=""):
+    """function to append"""
+    with open(filename, mode="a", encoding="utf_8") as a_file:
+        new_char_num = a_file.append(text)
+        return new_char_num
