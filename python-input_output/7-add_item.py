b90867a58aac0ed334fe138b2fe88a70b59f48d4
isamuella <s.ineza@alustudent.com>
2024-11-12 06:30:10 -0800

    ex7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,9 +4,10 @@
 
 import sys
 from os import path
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
-
+save_from_json_file = __import__('5-save_to_json_file')\
+        .save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file')\
+        .load_from_json_file
 filename = "add_item.json"
 if path.exists(filename):
     args = load_from_json_file(filename)
