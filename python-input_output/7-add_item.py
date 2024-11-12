785e6e6323f7fcf17f517a51fd82826cc67cdf9c
isamuella <s.ineza@alustudent.com>
2024-11-12 11:53:06 -0800

    ex7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,11 +6,13 @@ import sys
 from os import path
 save_from_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 filename = "add_item.json"
 if path.exists(filename):
     args = load_from_json_file(filename)
 else:
     args = []
+
 # Adds a new argument to the list
 args.extend(sys.argv[1:])
 # Saves the new list to the file
