7d08f8db6ff379ca3698b7584eb83aab11a364cb
isamuella <s.ineza@alustudent.com>
2024-11-12 11:40:22 -0800

    ex12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
     for x in range(1, n):
         row = [1]
         for y in range(1, x):
-            row.append(triangle[x-1][y-1] + triangle[x-1][j])
+            row.append(triangle[x-1][y-1] + triangle[x-1][y])
         row.append(1)
         triangle.append(row)
     return triangle
