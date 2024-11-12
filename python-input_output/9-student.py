a114f8c54612eaf4048e4b96676d7bf8afb96d3f
isamuella <s.ineza@alustudent.com>
2024-11-12 06:53:19 -0800

    ex9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -2,6 +2,7 @@
 """an empty class student"""
 
 
+
 class Student:
     """the class Student with attributes"""
     def __init__(self, first_name, last_name, age):
@@ -9,5 +10,5 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-def to_json(self):
-    return self.__dict__
+    def to_json(self):
+        return self.__dict__
