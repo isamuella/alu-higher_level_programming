888a21dc505ab3464ccb5ee9bdb86d38e54e7ad9
isamuella <s.ineza@alustudent.com>
2024-11-12 03:37:42 -0800

    ex2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 def append_write(filename="", text=""):
     """function to append"""
     with open(filename, mode="a", encoding="utf_8") as a_file:
-        new_char_num = a_file.append(text)
+        new_char_num = a_file.write(text)
         return new_char_num
