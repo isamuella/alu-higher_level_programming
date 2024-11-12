be987e4e1dbca9012d2ee6c73e5174285ac846e2
isamuella <s.ineza@alustudent.com>
2024-11-12 07:09:58 -0800

    ex10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,5 +13,7 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {key: value for key, value in self.__dict__.items() \
-                    if key in attrs}
+            return {
+                    key: value for key, value in self.__dict__.items()
+                    if key in attrs
+            }
