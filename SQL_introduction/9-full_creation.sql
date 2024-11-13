e7c9bb2c47eb7ade72e804215c6174f7b53f911f
isamuella <s.ineza@alustudent.com>
2024-11-13 04:43:13 -0800

    ex9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,8 @@
 -- Creating a new table in the same database
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 
-INSERT INTO second_table(id, name, score) VALUES
+INSERT INTO second_table(id, name, score)
+VALUES
 (1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
