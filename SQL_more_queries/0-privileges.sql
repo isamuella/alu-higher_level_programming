63250b4e9af975af5d8592135a45b9ea04b94cd3
isamuella <s.ineza@alustudent.com>
2024-11-13 05:59:00 -0800

    ex0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -3,7 +3,7 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
 GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_2'@'localhost';
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
