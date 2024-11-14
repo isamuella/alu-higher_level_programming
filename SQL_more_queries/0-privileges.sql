2a3a3882f62caeecc6d79606f337f52e41cca2f3
isamuella <s.ineza@alustudent.com>
2024-11-14 00:40:00 -0800

    ex0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,13 @@
 -- List all priviledges
+/*
+#!/bin/bash
+USERS = ('user_0d_1' 'user_0d_2')
+HOST = 'localhost'
 
+for USER in '${USERS[@]}'
+do
+	SHOW GRANTS FOR '$USER'@'$HOST';
+done
+*/
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
