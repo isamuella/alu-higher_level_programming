d18ae1d124db0abeadf2ca4ca839b057b4c8bbc3
isamuella <s.ineza@alustudent.com>
2024-11-14 06:21:12 -0800

    ex6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- Creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
+USE htbn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(id INT AUTO INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
