55b26f54fc75998aa4e32238d18cb6329520b7b4
isamuella <s.ineza@alustudent.com>
2024-11-14 04:03:19 -0800

    MSQL0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Lists all priviledges of the MYSQL users user_0d_1 and user_0s_2
--- Check if users exist and show their privileges
+-- Check if users exist and show their privilege
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
