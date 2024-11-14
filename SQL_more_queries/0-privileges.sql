4c9d1870135cdc94dc51b94d744b1d41aa7a8043
isamuella <s.ineza@alustudent.com>
2024-11-14 02:38:06 -0800

    ex0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,9 @@
--- Create users if they don't exist and grant privileges
+-- Create the users if they don't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Grant all privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
 
--- List privileges for each user
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
+-- Run the script to list the privileges
+SOURCE list-mysql-user-privileges;
