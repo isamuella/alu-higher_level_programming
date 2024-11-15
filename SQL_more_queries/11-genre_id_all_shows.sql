1c7a2224635c6f6d8da44ec107c7e5b3821e1ea8
isamuella <s.ineza@alustudent.com>
2024-11-15 01:08:38 -0800

    ex11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Show doesn’t have a genre, display NULL
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows_id = tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
