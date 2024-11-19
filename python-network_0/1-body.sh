baceaab9ed266abe9832a02676a80cd5eab5a0ea
isamuella <s.ineza@alustudent.com>
2024-11-19 12:38:56 +0000

    ex1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -w "%{http_code}" -o temp_response.txt | grep -q '200' && cat temp_response.txt
+curl -sL "$1" -w "%{http_code}" -o temp_response.txt | grep -q '200' && cat temp_response.txt
