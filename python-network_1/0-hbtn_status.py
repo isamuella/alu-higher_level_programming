044c31cf893ace99f0debb54dd2f0f65afda8a82
isamuella <s.ineza@alustudent.com>
2024-11-20 12:30:31 +0000

    ex0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     try:
         request = urllib.request.Request("https://alu-intranet.hbtn.io/status")
         with urllib.request.urlopen(request) as response:
