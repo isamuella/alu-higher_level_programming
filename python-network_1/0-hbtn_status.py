118c3ad5477222462ac57af744a0476be9f4b65e
isamuella <s.ineza@alustudent.com>
2024-11-20 12:02:36 +0000

    ex0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""Fetch https://intranet.hbtn.io/status."""
-from urllib.request import urlopen
+"""
+Module to fetch and display the response from https://alu-intranet.hbtn.io/status
+"""
+from urllib.request import urlopen, Request
 
 
-if __name__ == "__main__":    
-    try:
-        url = "https://intranet.hbtn.io/status"
-        with urlopen(url) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
-    except:
-        url = "https://intranet.hbtn.io/status"
-        with urlopen(url) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+    # Add a User-Agent header to the request
+    request = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+    
+    with urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
