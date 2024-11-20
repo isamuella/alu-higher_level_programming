e6903603a30c59d035db8c61568a1faac3ba6af2
isamuella <s.ineza@alustudent.com>
2024-11-20 14:23:32 +0000

    ex2

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Send POST request with email as parameter and display the response."""
+"""Takes in a URL and sends a POST email request and display the response."""
+import sys
 import urllib.request
 import urllib.parse
-import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-
-    # Create a dictionary with email as a parameter
     data = {'email': email}
-    # Encode the dictionary into a form-urlencoded format
+    
     data = urllib.parse.urlencode(data).encode('utf-8')
 
-    # Send POST request
+    # Sending a POST request
     with urllib.request.urlopen(url, data) as response:
         body = response.read()
         print(body.decode('utf-8'))
