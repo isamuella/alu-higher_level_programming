6ac539d335512451550c3bd8d021812d6e8fe28f
isamuella <s.ineza@alustudent.com>
2024-11-20 11:55:26 +0000

    ex0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,11 +3,21 @@
 from urllib.request import urlopen
 
 
-if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    with urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+if __name__ == "__main__":    
+    try:
+        url = "https://intranet.hbtn.io/status"
+        with urlopen(url) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+    except:
+        url = "https://intranet.hbtn.io/status"
+        with urlopen(url) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode("utf-8")))
