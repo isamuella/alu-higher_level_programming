7ff8a720fb74f3fe36d230801bc54c07cd15d63d
isamuella <s.ineza@alustudent.com>
2024-11-20 11:47:50 +0000

    ex0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Fetch https://intranet.hbtn.io/status."""
-import urllib.request
+from urllib.request import urlopen
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
+    url = "https://intranet.hbtn.io/status"
+    with urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
