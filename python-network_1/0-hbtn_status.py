cf21ccbde93eb1f81c5f054753fa62aa3675b024
isamuella <s.ineza@alustudent.com>
2024-11-20 12:26:43 +0000

    ex0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Fetch https://alu-intranet.hbtn.io/status."""
 import urllib.request
-
+from urllib.error import URLError, HTTPError
 
 if __name__ == "__main__":    
     try:
@@ -13,11 +13,15 @@ if __name__ == "__main__":
             print("\t- content: {}".format(body))
             print("\t- utf8 content: {}".format(body.decode("utf-8")))
 
-    except:
-        request = urllib.request.Request("http://0.0.0.0:5050/status")
-        with urllib.request.urlopen(request) as response: 
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    except (HTTPError, UURLError) as e:
+        print(f"Error occured: {e}")
+        try:
+            request = urllib.request.Request("http://0.0.0.0:5050/status")
+            with urllib.request.urlopen(request) as response: 
+                 body = response.read()
+                 print("Body response:")
+                 print("\t- type: {}".format(type(body)))
+                 print("\t- content: {}".format(body))
+                 print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    except Exceptio as e:
+        print(f"Error ocurred shile fetching secondary URL: {e}")
