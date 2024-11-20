99287825f6a88348e76e7a867005e8aee53c5e3f
isamuella <s.ineza@alustudent.com>
2024-11-20 14:27:07 +0000

    ex2

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {'email': email}
-    
+
     data = urllib.parse.urlencode(data).encode('utf-8')
 
     # Sending a POST request
