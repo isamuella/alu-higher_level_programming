178e69aa21c4adf45f25c1d8791d5444b714a365
isamuella <s.ineza@alustudent.com>
2024-11-21 01:36:19 -0800

    ex5

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Sends a request to URL and display the value variable in the header"""
+import sys
+import requests
+
+if __name__ == "__main__":
+url = sys.arg[1]
+value = requests.get(url)
+print(value.headers.get("X-Request-Id"))
