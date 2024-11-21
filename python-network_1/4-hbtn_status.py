3f8c3461cff24fccbd0baf489624195e56b18d45
isamuella <s.ineza@alustudent.com>
2024-11-21 01:27:05 -0800

    ex4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,14 +4,8 @@ import requests
 
 
 if __name__ == "__main__":
-   try:
-       url = "https://alu-intranet.hbtn.io/status"
-       value = requests.get(url)
-       value.raise_for_status()
-   except requests.exceptions.RequestException:
-       url = "http://0.0.0.0:5050/status"
-       value = requests.get(url)
-
+    url = "https://alu-intranet.hbtn.io/status"
+    value = requests.get(url)
     status = value.text.strip()
 
     print("Body response:")
