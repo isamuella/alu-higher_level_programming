6a25e6987630f7a2febdb947a2d2a0f1a01223f8
isamuella <s.ineza@alustudent.com>
2024-11-21 02:32:20 -0800

    ex9

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     url = "https://api.github.com/user"
-    
+
     auther = (username, password)
     try:
         value = requests.get(url, auth=auther)
