697b408cfece73b138daed2df9a7051ef7f3fa98
isamuella <s.ineza@alustudent.com>
2024-11-21 02:30:28 -0800

    ex9

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Use the GitHub API to display your id from my GitHub credentials"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+    
+    auther = (username, password)
+    try:
+        value = requests.get(url, auth=auther)
+        value.raise_for_status()
+        print(value.json().get("id"))
+    except requests.exceptions.RequestException:
+        print("None")
