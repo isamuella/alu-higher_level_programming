7f55a537cfa2325b547c9c620bae123d26148298
isamuella <s.ineza@alustudent.com>
2024-11-21 02:42:41 -0800

    ex4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-"""Fetch status from a URL and display the content."""
-
+"""Fetch https://alu-intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
-    # Define both URLs for testing purposes
-    url = "https://alu-intranet.hbtn.io/status"  # For the production test
-    fallback_url = "http://0.0.0.0:5050/status"  # For the local test
-    
-    # Fetch content from the production URL first
-    try:
-        value = requests.get(url)
-        value.raise_for_status()  # Raise error if the request fails
-    except requests.exceptions.RequestException:
-        # If the production URL fails, fallback to the local URL
-        value = requests.get(fallback_url)
-
-    # Extract and print the response body
+    url = "https://alu-intranet.hbtn.io/status"
+    value = requests.get(url)
     body = value.text
 
     print("Body response:")
