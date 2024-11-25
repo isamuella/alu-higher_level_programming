f9399a8fbe5abb40d4d507cf9a9ccbd6964ae77a
isamuella <s.ineza@alustudent.com>
2024-11-25 02:46:10 -0800

    ex1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -19,13 +19,13 @@ def matrix_divided(matrix, div):
          A new matrix with the division answer
     """
     if not isinstance(matrix, list) or not all(
-            isinstance(row, list) for row in matrix
+        isinstance(row, list) for row in matrix
     ):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    if not all(isinstance(element, (int, float))
-            for row in matrix
-            for element in row):
+    if not all(
+        isinstance(element, (int, float)) for row in matrix for element in row
+    ):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats"
         )
