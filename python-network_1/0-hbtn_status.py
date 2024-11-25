3bee7f821db58333c92455be25c0dc8098dd62fc
isamuella <s.ineza@alustudent.com>
2024-11-25 01:45:02 -0800

    ex0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
         "https://alu-intranet.hbtn.io/status"
         if "https://intranet.hbtn.io/status".statswith("https")
         else "https://intranet.hbtn.io/status") as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", body.decode("utf-8"))
+     body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode("utf-8"))
