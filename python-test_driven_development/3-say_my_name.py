832532609d68bb508952c8e4a81c0f5407cfb820
isamuella <s.ineza@alustudent.com>
2024-11-25 03:16:33 -0800

    ex2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
 Print square with character #
 """
 
+
 def say_my_name(first_name, last_name=""):
     """A function that prints My name is <first name> <last name>"""
     if not isinstance((first_name), str):
