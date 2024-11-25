e75d2eb3aa77a7ad79e70b1adc944f46f664d912
isamuella <s.ineza@alustudent.com>
2024-11-25 03:54:27 -0800

    test5

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
-add unittests
-"""
 
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TextMaxInteger(unittest.TestCase):
+    def test_integer(self):
+        # Test if the list is empty
+        self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1,5,3,9]), 9)
+        self.assertAlmostEqual(max_integer([10,5,2,8]), 10)
+        self.assertAlmostEqual(max_integer([5,8,3]), 8)
+        self.assertAlmostEqual(max_integer([4,-8,19,2]), 19)
+        self.assertAlmostEqual(max_integer([-101,-45,-78,-96,-10,-175]), -10)
+        self.assertAlmostEqual(max_integer([6]), 6)
