7e5a054e473b8eafe40869ce609bf3f58917f718
isamuella <s.ineza@alustudent.com>
2024-11-25 01:18:36 -0800

    ex0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,22 +2,11 @@
 """Fetch https://alu-intranet.hbtn.io/status."""
 import urllib.request
 
+url = "https://alu-intranet.hbtn.io/status"
 
-if __name__ == "__main__":
-    try:
-        request = urllib.request.Request("https://alu-intranet.hbtn.io/status")
-        with urllib.request.urlopen(request) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
-    except:
-        request = urllib.request.Request("http://0.0.0.0:5050/status")
-        with urllib.request.urlopen(request) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))
+with urllib.request.urlopen(request) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
