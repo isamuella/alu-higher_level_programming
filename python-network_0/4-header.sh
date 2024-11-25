ec579ed2b90c6dc67465c8ff9a138951a3ff09e6
isamuella <s.ineza@alustudent.com>
2024-11-25 01:10:37 -0800

    ex4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL as an argument, sends a GET request to the URL, and displays the body
-curl -sH "X-HolbertonSchool-User-Id: 98" -o response body.txt "$1" && cat response_body.txt
+curl -s -H "X-HolbertonSchool-User-Id: 98" -o response body.txt "$1" && cat response_body.txt
