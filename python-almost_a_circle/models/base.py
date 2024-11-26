1b1967db98aaeedb2b0a2e212a8e4379bdd28533
isamuella <s.ineza@alustudent.com>
2024-11-26 02:16:46 -0800

    ex1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A first class Base
+"""
+
+
+class Base:
+    """class Base to manage id attribute"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize the Base instance
+
+        Args:
+            id(int): An id instance
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_object += 1
+            self.id = Base.__nb_objects
