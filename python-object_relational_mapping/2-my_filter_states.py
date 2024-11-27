aeb247cb2e774f2941cda4d417c2756104d9550f
isamuella <s.ineza@alustudent.com>
2024-11-27 01:40:20 -0800

    ex2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,8 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC;".format(state_name)
+        "SELECT * FROM states WHERE BINARY name = '{}'"
+        "ORDER BY id ASC;".format(state_name)
     )
 
     states = cursor.fetchall()
