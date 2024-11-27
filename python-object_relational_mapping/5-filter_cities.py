f1a1061c82b5da33a2a06b61f7fbf69a5b2f9912
isamuella <s.ineza@alustudent.com>
2024-11-27 02:33:39 -0800

    ex5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Takes state name as argument and lists all cities using the database"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    passwdord = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQldb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    cursor = db_cursor()
+
+    cursor.execute(
+        """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.states_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC;
+        """, (state_name,)
+    )
+
+    cities = cursor.fetchall()
+    if cities:
+        print(", ".join(city[0] for city in cities))
+
+    cursor.close()
+    db.close()
