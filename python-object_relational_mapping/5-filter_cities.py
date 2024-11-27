b1a6ca9fd80591d219b630711d835afe9c75cf51
isamuella <s.ineza@alustudent.com>
2024-11-27 02:43:43 -0800

    ex5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         """
         SELECT cities.name
         FROM cities
-        JOIN states ON cities.states_id = states.id
+        JOIN states ON cities.state_id = states.id
         WHERE states.name = %s
         ORDER BY cities.id ASC;
         """, (state_name,)
