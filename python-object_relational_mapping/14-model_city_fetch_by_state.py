b67edfda3001a6d846e6b93529560edfd205564a
isamuella <s.ineza@alustudent.com>
2024-11-29 02:50:04 -0800

    ex14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that prints all City objects from the database"""
+"""Prints all City objects from the database"""
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
