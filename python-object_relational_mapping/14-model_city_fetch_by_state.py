88036d078e285485e54fb58f8fae9b81aeaf5463
isamuella <s.ineza@alustudent.com>
2024-11-29 02:29:12 -0800

    ex14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """Script that prints all City objects from the database"""
-import sys
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+import sys
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
