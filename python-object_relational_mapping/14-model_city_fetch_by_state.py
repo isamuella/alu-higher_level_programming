151ab9a4601b02cf3b784673fdfa83e998761002
isamuella <s.ineza@alustudent.com>
2024-11-29 02:30:33 -0800

    ex14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
- 
+
     cities = session.query(State, City).filter(
         State.id == City.state_id).order_by(City.id)
     for state, city in cities:
