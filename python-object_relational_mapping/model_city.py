bb1dd0356c43a09a995e0d736cc041f7307e728c
isamuella <s.ineza@alustudent.com>
2024-11-29 02:49:30 -0800

    ex14

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Contains City class"""
+"""Contains the class City"""
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
+
 class City(Base):
-    """Class representing cities table"""
+    """A class that represents cities table"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
