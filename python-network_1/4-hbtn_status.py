94b47ca3f2025d1e884be4d5e1576accf773822d
isamuella <s.ineza@alustudent.com>
2024-11-30 20:54:17 -0800

    ex4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,7 +5,13 @@ import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status")
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+        AppleWebKit/537.36 (KHTML, like Gecko) 
+        Chrome/91.0.4472.124 Safari/537.36'
+    }
+
+    response = requests.get("https://intranet.hbtn.io/status", headers=headers)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
