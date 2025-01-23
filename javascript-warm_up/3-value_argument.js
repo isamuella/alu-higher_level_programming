48ab626facdd37784f38c7e341108a412a296310
isamuella <s.ineza@alustudent.com>
2025-01-23 04:13:25 -0800

    ex3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-const args = process.argv.slice(2)
-for (args.length === 0)
-	console.log('No argument');
+if (process.argv[2]) {
+  console.log(process.argv[2]);
+} else {
+  console.log('No argument');
+}
