a2f5d23043151ebabb4435ac9f6faaddd0968fdb
isamuella <s.ineza@alustudent.com>
2025-01-23 05:09:58 -0800

    ex5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const num = Number(Arg1);
 if (isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ${Math.floor(num)}');
+  console.log(`My number: ${Math.floor(num)}`);
 }
