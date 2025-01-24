14225b8c13de4b508885f4d1206e99cdc81a200e
isamuella <s.ineza@alustudent.com>
2025-01-24 01:36:21 -0800

    ex8

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const square = Math.floor(Number(process.argv[2]))
+
+if (isNaN(square)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < square; i++) {
+    console.log('X'.repeat(square));
+  }
+}
