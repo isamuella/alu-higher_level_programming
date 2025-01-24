8c06da7a4568a3be04418c560d09bfe8b11e92bd
isamuella <s.ineza@alustudent.com>
2025-01-24 00:25:06 -0800

    ex7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const x = parseInt(process.argv[2]);
 
-if (isNaN(x)) {
-  console.log('Missing number of occurences');
-} else {
+if (x) {
   for  (let y = 0; y < x; y++) {
     console.log('C is fun');
   }
+} else {
+  console.log('Missing number of occurences');
 }
