9d70dd509be34cbeaf323d19a2ae38ba27d06634
isamuella <s.ineza@alustudent.com>
2025-01-24 00:37:15 -0800

    ex7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
-
-if (x) {
-  for  (let y = 0; y < process.argv[2]; y++) {
+const num = parseInt(process.argv[2]);
+if (num) {
+  for (let i = 0; i < process.argv[2]; i++) {
     console.log('C is fun');
   }
 } else {
