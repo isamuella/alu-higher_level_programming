4b445ec6a6f9eeb65eedc478763c67af60b58372
isamuella <s.ineza@alustudent.com>
2025-01-24 05:17:11 -0800

    ex10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 function factorial (num) {
-  if(isNaN(num) || num <= 1) {
+  if (isNaN(num) || num <= 1) {
     return 1;
   }
   return num * fcactorial(num - 1);
 }
-console.log(factorial(paseInt(process.argv[2])));
+console.log(factorial(parseInt(process.argv[2])));
