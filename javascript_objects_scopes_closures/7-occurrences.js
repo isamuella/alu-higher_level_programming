afc5bc4405f63299dcd52f171b7f2a447f8585c4
isamuella <s.ineza@alustudent.com>
2025-01-29 04:58:07 -0800

    ex7

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 exports.nbOccurences = function (list, searchElement) {
-return list.filter(element => element === searchElement).length;
+  return list.filter(element => element === searchElement).length;
 };
