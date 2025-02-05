17dcefb099524c2705270f7fa2b6a6d977a1d481
isamuella <s.ineza@alustudent.com>
2025-02-05 13:05:19 +0000

    ex0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, info) {
-  console.log(error || info);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(info);
+  }
 });
