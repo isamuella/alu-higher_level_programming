e127f1d34e19cea2ee03c63aaf0f15e89797ffcd
isamuella <s.ineza@alustudent.com>
2025-02-06 16:19:46 +0000

    ex4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,5 +3,5 @@ const request = require('request');
 request(process.argv[2], (error, _, body) => {
   if (error) return console.error(error);
   console.log(JSON.parse(body).results.filter(
-  f => f.characters.find(c => c.includes('/18/'))).length);
+    f => f.characters.find(c => c.includes('/18/'))).length);
 });
