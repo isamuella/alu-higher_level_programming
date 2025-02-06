a3abfb038e83563240bdbb494594fb258673f708
isamuella <s.ineza@alustudent.com>
2025-02-06 17:09:18 +0000

    ex6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
-const request = require('require');
-request(process.argv[2], (err, _, body) => {
-  if (err) return console.error(err);
-
-  const completedTasks = JSON.parse(body).reduce((acc, todo) => {
-    if (todo.completed) acc[todo.userId] = (acc[todo.userId] || 0) + 1;
-    return acc;
-  }, {});
-
-  console.log(completedTasks);
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
+  }
 });
