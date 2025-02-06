a3bccee58e4a44b7f7721fdbd93c2c82f53dc4a9
isamuella <s.ineza@alustudent.com>
2025-02-06 16:43:52 +0000

    ex5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
-request(process.argv[2], (error, _, body) => {
-  if (error) return console.errror(error);
-  fs.writeFile(process.argv[3], body, 'utf-8', console.error);
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
